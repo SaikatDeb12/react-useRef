d6ce172f85266db83439d236da5f385f92de1140
SaikatDeb12 <saikat.rcld@gmail.com>
2025-06-13 17:23:38 +0530

    using useRef

---
+++ src/Stopwatch2.jsx
@@ -0,0 +1,28 @@
+import { useRef, useState } from "react";
+
+const Stopwatch2 = () => {
+  const [currentCount, setCurrentCount] = useState(1);
+  const timer = useRef();
+
+  const startClock = () => {
+    let value = setInterval(() => {
+      setCurrentCount((val) => val + 1);
+    }, 1000);
+
+    //just like useState the value of the timer is persistent across at re-rendering
+    // and the change of this value does not trigger any re-render
+    timer.current = value;
+  };
+
+  const stopClock = () => {
+    clearInterval(timer.current);
+  };
+
+  return (
+    <div>
+      {currentCount}
+      <button onClick={startClock}>Start</button>
+      <button onClick={stopClock}>Stop</button>
+    </div>
+  );
+};
